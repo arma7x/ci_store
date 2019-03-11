c5eba2853e66cc87fe211a58b7811fcd24da8400
arma7x <arma7x@live.com>
2019-03-11 15:01:44 +0800

    local update

--- application/views/widgets/dashboard/menu.php
+++ application/views/widgets/dashboard/menu.php
@@ -31,6 +31,7 @@
                       <a class="dropdown-item p-2<?php echo $this->uri->segment(2) == 'manage_user' ? ' active' : ''?>" onclick="navigate('/dashboard/manage_user')"><i class="material-icons">&#xe2c9;</i> <?php echo lang('H_MANAGE_USERS');?></a>
                       <div class="dropdown-divider"></div>
                       <a class="dropdown-item p-2<?php echo '/'.$this->uri->uri_string() == '/dashboard/general_information' ? ' active' : ''?>" onclick="navigate('/dashboard/general_information')"><i class="material-icons">&#xe88e;</i> <?php echo lang('H_GENERAL_INFORMATION');?></a>
+                      <div class="dropdown-divider"></div>
                       <a class="dropdown-item p-2<?php echo '/'.$this->uri->uri_string() == '/dashboard/essential_information' ? ' active' : ''?>" onclick="navigate('/dashboard/essential_information')"><i class="material-icons">&#xe873;</i> <?php echo lang('H_ESSENTIAL_INFORMATION');?></a>
                       <div class="dropdown-divider"></div>
                       <a class="dropdown-item p-2<?php echo '/'.$this->uri->uri_string() == '/dashboard/category' ? ' active' : ''?>" onclick="navigate('/dashboard/category')"><i class="material-icons">&#xe54e;</i> <?php echo lang('H_CATEGORY');?></a>
