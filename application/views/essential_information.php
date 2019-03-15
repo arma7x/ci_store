6001421ba2de238e25ea0c36f5e8a65a91fdefb9
arma7x <arma7x@live.com>
2019-03-15 12:20:08 +0800

    local update

--- application/views/essential_information.php
+++ application/views/essential_information.php
@@ -1,10 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 
 <div class="container bg-light">
-	<div class="starter-template">
-		<div class="col col-12">
-			<h2 class="text-center text-primary"><?php echo isset($page_name) ? strtoupper($page_name) : strtoupper('Codeigniter') ;?></h2>
-		</div>
-		<div><?php echo $ei_content['full_description'] ?></div>
+	<div class="col col-12">
+		<h2 class="text-center text-primary"><?php echo isset($page_name) ? strtoupper($page_name) : strtoupper('Codeigniter') ;?></h2>
 	</div>
+	<div><?php echo $ei_content['full_description'] ?></div>
 </div>
