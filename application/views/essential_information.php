437b1f55c330a26d1a80a4bbcf54f2aedc7fec92
arma7x <arma7x@live.com>
2019-03-22 11:07:51 +0800

    update readme

--- application/views/essential_information.php
+++ application/views/essential_information.php
@@ -2,7 +2,7 @@
 
 <div class="container">
 	<div class="starter-template" style="padding-right:0;padding-left:0;">
-		<h2 class="text-center text-primary"><?php echo isset($page_name) ? strtoupper($page_name) : strtoupper('Codeigniter') ;?></h2>
+		<h2 class="my-2 text-center text-primary"><?php echo isset($page_name) ? strtoupper($page_name) : strtoupper('Codeigniter') ;?></h2>
 		<div><?php echo $ei_content['full_description'] ?></div>
 	</div>
 </div>
